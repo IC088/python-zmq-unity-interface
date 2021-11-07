432f8171f06737acb02f706c1e91f631cda79f60
IC088 <33095731+IC088@users.noreply.github.com>
2021-11-07 17:21:46 +0800

    Update publisher.py

--- utils/publisher.py
+++ utils/publisher.py
@@ -10,9 +10,17 @@ import numpy as np
 
 class Publihser():
 	def __init__(self, port = 8989):
+		'''
+		Initialise publisher to publish face coordinate data
+
+
+		Args:
+		- port : int (specify)
+		'''
 		self.context = zmq.Context()
 		self.socket = self.context.socket(zmq.PUB)
 
 		self.socket.bind(f'tcp://*:{port}')
-	def publish_data(self):
-		pass
+
+	def publish_data(self, message):
+		print(message)
